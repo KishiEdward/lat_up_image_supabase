a03f4186c4f4ce56eb5f9c66be38f4f8c9663ce4
KishiEdward <dzidanrh@gmail.com>
2025-12-12 22:41:47 +0700

    mengubah main.dart dan menambah dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:supabase_image/homepage.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://poetiavbyzulpxvtyrej.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvZXRpYXZieXp1bHB4dnR5cmVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1MzkxMjAsImV4cCI6MjA4MTExNTEyMH0.nAD1HL6-EUaT40HVS-pYVxyvwIIeyztV0Mer-1XfXyY',
+  );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
