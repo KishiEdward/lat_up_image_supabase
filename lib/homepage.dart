5b680769885d784b1e4d42b3f4004ab5224baa80
KishiEdward <dzidanrh@gmail.com>
2025-12-12 23:22:52 +0700

    final bisa upload gambar dengan sedikit modifikasi

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,10 +1,116 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final supabase = Supabase.instance.client;
+
+  String? _publicImageUrl;
+  bool _isUploading = false;
+
+  Future<void> _pickAndUploadToPublicBucket() async {
+    final picker = ImagePicker();
+
+    // memilih gambar
+    final picked = await picker.pickImage(source: ImageSource.gallery);
+    if (picked == null) {
+      return;
+    }
+    setState(() {
+      _isUploading = true;
+    });
+
+    try {
+      // membuat nama file unik
+      final fileName =
+          '${DateTime.now().millisecondsSinceEpoch}_${picked.name}';
+      final filePath = 'uploads/$fileName';
+
+      if (kIsWeb) {
+        // untuk web
+        final bytes = await picked.readAsBytes();
+        await supabase.storage
+            .from('bucket_images')
+            .uploadBinary(
+              filePath,
+              bytes,
+              fileOptions: const FileOptions(contentType: 'image/jpg'),
+            );
+      } else {
+        // untuk mobile
+        final file = File(picked.path);
+        await supabase.storage.from('bucket_images').upload(filePath, file);
+      }
+
+      // ambil URL publik
+      final publicUrl = supabase.storage
+          .from('bucket_images')
+          .getPublicUrl(filePath);
+      setState(() {
+        _publicImageUrl = publicUrl;
+      });
+    } catch (e) {
+      debugPrint('Error upload: $e');
+      if (mounted) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text('Gagal upload: $e')));
+      }
+    } finally {
+      if (mounted) {
+        setState(() => _isUploading = false);
+      }
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Supabase Image Upload')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            // menampilkan progress indicator jika sedang upload
+            if (_isUploading) const LinearProgressIndicator(),
+
+            const SizedBox(height: 16),
+
+            // button untuk pilih image
+            ElevatedButton(
+              onPressed: _isUploading ? null : _pickAndUploadToPublicBucket,
+              child: const Text('Pilih & Upload Gambar'),
+            ),
+
+            const SizedBox(height: 24),
+
+            // menampilkan Image yang kita upload
+            // kika _publicImageUrl tidak null, maka tampilkan list widget di bawah ini
+            if (_publicImageUrl != null) ...[
+              const Text('Gambar dari Public URL:'),
+              const SizedBox(height: 8),
+
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: Image.network(_publicImageUrl!),
+                ),
+              ),
+            ],
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:supabase_image/homepage.dart';
+import 'homepage.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,6 +9,7 @@ Future<void> main() async {
     anonKey:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvZXRpYXZieXp1bHB4dnR5cmVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1MzkxMjAsImV4cCI6MjA4MTExNTEyMH0.nAD1HL6-EUaT40HVS-pYVxyvwIIeyztV0Mer-1XfXyY',
   );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
